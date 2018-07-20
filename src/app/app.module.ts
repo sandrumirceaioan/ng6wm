69f04512dfbb53bd2cb08ea91a89dc95e67c1dd0
DESKTOP-G38F96I\pircea <mircea.sandru@gosocialdev.eu>
2018-07-20 20:55:59 +0300

    login template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 /* main components */
 import { LoginComponent } from './login/login.component';
-
 
 /* routing module */
 import { AppRoutingModule } from './app.routing';
@@ -22,6 +22,7 @@ import { AppRoutingModule } from './app.routing';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     ToastrModule.forRoot(),
     FontAwesomeModule,
     AppRoutingModule,
