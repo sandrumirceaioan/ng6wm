8b0e5dcbd1b904b48c480d7ea48cadda0d32fe8f
Sandru Mircea Ioan <sandrumirceaioan@gmail.com>
2018-08-22 01:13:45 +0300

    something

--- src/app/add-tasks/add-tasks.component.ts
+++ src/app/add-tasks/add-tasks.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../project/project.model';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { ProjectsService } from '../shared/services/projects/projects.service';
+import { Router } from '@angular/router';
+
+library.add(faChevronLeft);
 
 @Component({
   selector: 'app-add-tasks',
@@ -6,10 +13,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-tasks.component.scss']
 })
 export class AddTasksComponent implements OnInit {
-
-  constructor() { }
+  task = {};
+  projects: Project[];
+  constructor(
+    private projectsService: ProjectsService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.projects = this.projectsService.projects;
+    console.log( this.projects);
+    this.task['taskProject'] = null;
+  }
+
+  back() {
+    this.router.navigate(['/manage/tasks']);
   }
 
 }
